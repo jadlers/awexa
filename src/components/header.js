ca2b283263ddc983d03615bc9faf95300c0e7a9f
Jacob Adlers <jacob.adlers@gmail.com>
2018-11-20 14:51:17 +0100

    Remove provided styles and use styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const PageTitle = styled.h1`
+  margin: 0;
+  font-size: 2.25em;
+  text-align: center;
+`;
 
 const Header = ({ siteTitle }) => (
   <div style={{ marginBottom: '1.45rem', padding: '1.45em 1.1em' }}>
-    <h1 style={{ margin: 0, textAlign: 'center' }}>{siteTitle}</h1>
+    <PageTitle>{siteTitle}</PageTitle>
   </div>
 );
 
