489618191be8a96fb4b998871e3b25d5bd642b2b
Jacob Adlers <jacob.adlers@gmail.com>
2018-11-20 14:30:13 +0100

    Make header only be name of page

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,8 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <div style={{ marginBottom: '1.45rem', padding: '1.45em 1.1em' }}>
+    <h1 style={{ margin: 0, textAlign: 'center' }}>{siteTitle}</h1>
   </div>
 );
 
