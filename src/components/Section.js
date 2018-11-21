0d9a44b217427c91a781dcf9e02902aba2a3b170
Jacob Adlers <jacob.adlers@gmail.com>
2018-11-21 15:51:40 +0100

    Structure mock data as it'll come later

--- src/components/Section.js
+++ src/components/Section.js
@@ -20,12 +20,23 @@ const Section = () => (
   </>
 );
 
-// TODO: Take these in as props
+// TODO: This should be sourced from the Netlify CMS
 const title = 'Vilka är vi?';
-const paragraphs = [
-  'Vi på Awexa hjälper dig att komma in på rätt spår. Vi har lång erfarenhet (64 år) av att se till så du gör rätt saker i rätt tid på rätt plats.',
-  'Allt ifrån din jobbkarriär till hur du fungerar i alla sammanhang. Ditt liv ska fungera på alla plan och vi ser till att det gör så.',
-  'Känner Du att ditt liv inte fungerar som det ska hör du bara av dig så bokar vi in en timmes första samtal.',
-];
+const content = (
+  <>
+    <p>
+      Vi på Awexa hjälper dig att komma in på rätt spår. Vi har lång erfarenhet
+      (64 år) av att se till så du gör rätt saker i rätt tid på rätt plats.
+    </p>
+    <p>
+      Allt ifrån din jobbkarriär till hur du fungerar i alla sammanhang. Ditt
+      liv ska fungera på alla plan och vi ser till att det gör så.
+    </p>
+    <p>
+      Känner Du att ditt liv inte fungerar som det ska hör du bara av dig så
+      bokar vi in en timmes första samtal.
+    </p>
+  </>
+);
 
 export default Section;
