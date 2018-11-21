e277da60d2e218097309a9714f51007e57e94f90
Jacob Adlers <jacob.adlers@gmail.com>
2018-11-21 16:27:29 +0100

    Mock section data in index.js and render there

--- src/components/Section.js
+++ src/components/Section.js
@@ -28,28 +28,9 @@ const Content = styled.div({
 
 const Section = props => (
   <SectionWrapper {...props}>
-    <Title>{title}</Title>
-    <Content>{content}</Content>
+    <Title>{props.title}</Title>
+    <Content>{props.content}</Content>
   </SectionWrapper>
-);
-
-// TODO: This should be sourced from the Netlify CMS
-const title = 'Vilka är vi?';
-const content = (
-  <>
-    <p>
-      Vi på Awexa hjälper dig att komma in på rätt spår. Vi har lång erfarenhet
-      (64 år) av att se till så du gör rätt saker i rätt tid på rätt plats.
-    </p>
-    <p>
-      Allt ifrån din jobbkarriär till hur du fungerar i alla sammanhang. Ditt
-      liv ska fungera på alla plan och vi ser till att det gör så.
-    </p>
-    <p>
-      Känner Du att ditt liv inte fungerar som det ska hör du bara av dig så
-      bokar vi in en timmes första samtal.
-    </p>
-  </>
 );
 
 export default Section;
